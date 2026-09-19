d197ff420d00ec5b0d12bcc7e165e0b138e85e9a
PherKryvko <phercontact@proton.me>
2026-09-19 09:22:56 +0100

    New line indentation and better printing and splitting

--- python_sentence_inspector.py
+++ python_sentence_inspector.py
@@ -1,5 +1,5 @@
 storing_sentence = """
-Sentence in a variable.
+Sentence in a variable. 
 Stored."""
 
 def print_char_count(sentence):
@@ -9,7 +9,7 @@ def first_last_char(sentence):
     print(f"First char: {sentence[0]}, and last char: {sentence[-1]}")
 
 def split_words_from_sentence(sentence):
-    split_words = sentence.split(" ")
+    split_words = sentence.split()
 
     for index, word in enumerate(split_words):
         print(f"{index} position for word: {word}")
@@ -19,7 +19,13 @@ def reverse_sentence(sentence):
 
 
 if __name__ == "__main__":
-    print(print_char_count(storing_sentence))
-    print(first_last_char(storing_sentence))
-    print(split_words_from_sentence(storing_sentence))
-    print(reverse_sentence(storing_sentence))
+    print_char_count(storing_sentence)
+
+    print("\nNEXT:")
+    first_last_char(storing_sentence)
+
+    print("\nNEXT:")
+    split_words_from_sentence(storing_sentence)
+
+    print("\nNEXT:")
+    reverse_sentence(storing_sentence)
