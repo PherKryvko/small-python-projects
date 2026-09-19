90c7433ba765e7e9606415ec21002c432f67cdcc
PherKryvko <phercontact@proton.me>
2026-09-19 14:51:02 +0100

    Q3 leetcode completed

--- leetcode_c1_arrays_strings_iteration.py
+++ leetcode_c1_arrays_strings_iteration.py
@@ -1,14 +1,4 @@
 """
-
-
-
-
-3: Given:
-
-nums = [7, 2, 10, 4]
-
-find the largest value manually using iteration.
-
 4: Given:
 
 nums = [1, 3, 5, 8]
@@ -67,6 +57,26 @@ class QuestionTwo():
 
         return tally
 
+"""
+3: Given:
+
+nums = [7, 2, 10, 4]
+
+find the largest value manually using iteration.
+"""
+
+class QuestionThree():
+
+    @staticmethod
+    def largest_num(num_array):
+        current_largest = num_array[0]
+
+        for nums in num_array:
+            if current_largest < nums:
+                current_largest = nums
+                
+        return current_largest
+
 if __name__ == "__main__":
     nums = [3, 5, 2, 8, 1]
 
@@ -75,6 +85,11 @@ if __name__ == "__main__":
     print(result)
 
     result = QuestionTwo.s_count("mississippi")
+
+    print(result)
+
+    nums_2 = [7, 2, 10, 4]
+    result = QuestionThree.largest_num(nums_2)
 
     print(result)
 
